7816d905ef1565d360a800f15652b8bd79fb78b8
YASIR AHMAD <yazirahmed123@gmail.com>
2025-02-27 05:26:30 +0300

    added user column in models.py

--- todo/models.py
+++ todo/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
 class Tasks(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     status = models.BooleanField(default=False)
-    due_date = models.DateField(auto_now=True)
+    due_date = models.DateField(blank=True , null=True)
+    user = models.ForeignKey(User , on_delete=models.CASCADE , related_name="tasks" , null=True)
+    
 
     # return string instead of class
     def __str__(self):
