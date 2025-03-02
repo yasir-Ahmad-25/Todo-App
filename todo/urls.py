370547078f86f6ff82674c7fa3ce67f71b8e4315
YASIR AHMAD <yazirahmed123@gmail.com>
2025-03-02 05:21:08 +0300

    added update delete functionalities

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index,createTodo
+from .views import index,createTodo,updateTodo,deleteTodo
 
 urlpatterns = [
     path('' , index , name="index"),
-    path('create_todo' , createTodo , name="create-todo")
+    path('create_todo' , createTodo , name="create-todo"),
+    path('update_todo/<int:pk>' , updateTodo , name="update-todo"),
+    path('delete_todo/<int:pk>' , deleteTodo , name="delete-todo"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 
 from .models import Tasks
 from .forms import TodoForm
 # Create your views here.
 def index(request):
-    return render(request, 'todo/todo_list.html' , {})
+    todos = Tasks.objects.all()
+    context = {
+        'todos' : todos
+    }
+    return render(request, 'todo/todo_list.html' , context)
 
 # create Task Form
 def createTodo(request):
@@ -20,3 +24,27 @@ def createTodo(request):
             'form' : form
         }
         return render(request , 'todo/create_task.html', context)
+ 
+
+# update Task Form
+def updateTodo(request,pk):
+    # if request.user.is_authenticated:
+    object = get_object_or_404(Tasks , pk=pk)
+    if request.method == "POST":
+        form  = TodoForm(request.POST , instance=object)
+        if form.is_valid():
+            form.user = request.user
+            form.save()
+            return redirect('index')
+    else:
+        context = {
+            'form' : TodoForm(instance=object)
+        }
+    return render(request , 'todo/update_task.html' , context)
+
+
+# delete Task Form
+def deleteTodo(request,pk):
+    todo = get_object_or_404(Tasks , pk=pk)
+    todo.delete()
+    return redirect('index')
